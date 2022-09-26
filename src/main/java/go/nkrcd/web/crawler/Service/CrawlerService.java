a9629c9f83690f93785396174a6c735c08858839
yewon-Noh <yewo2nn16@gmail.com>
2022-09-27 00:53:49 +0900

    add : 회사 주소 크롤러
    회사 주소를 가져오기 위해 회사 주소 크롤러를 생성하여 회사 목록 크롤링 시 사용하도록 함
    
    (결과 예시)
    - 기존 주소 : 인천
    - 회사 주소 크롤러 후 : 인천 미추홀구 인하로 120

--- src/main/java/go/nkrcd/web/crawler/Service/CrawlerService.java
+++ src/main/java/go/nkrcd/web/crawler/Service/CrawlerService.java
@@ -32,9 +32,15 @@ public class CrawlerService {
     @Value("${crawler.fin.url}")
     private String finURL;
 
+    @Value("${crawler.address.url}")
+    private String addressURL;
+
     @Value("${crawler.driver}")
     private String driver;
 
+    /*
+     * 회사 목록 크롤링
+     */
     public boolean list() {
         List<Company> result = new ArrayList<>();
 
@@ -64,8 +70,8 @@ public class CrawlerService {
             List<WebElement> contents = driver.findElements(By.cssSelector("div.section_group > section"));
             if (contents.size() > 0) {
                 for (WebElement content : contents) {
-                    String name = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dt > a")).getText().replace("(주)", "");
-                    String address = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dd:nth-child(2) > span:nth-child(3)")).getText();
+                    String name = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dt > a")).getText().replace("(주)", "").replace("(유)", "");
+                    String address = getAddress(name);
                     String logo = content.findElement(By.cssSelector("div > div > div > a > img")).getAttribute("src");
                     result.add(new Company(name, address, logo));
                 }
@@ -89,8 +95,8 @@ public class CrawlerService {
                     contents = driver.findElements(By.cssSelector("div.section_group > section"));
                     if (contents.size() > 0) {
                         for (WebElement content : contents) {
-                            String name = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dt > a")).getText().replace("(주)", "");
-                            String address = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dd:nth-child(2) > span:nth-child(3)")).getText();
+                            String name = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dt > a")).getText().replace("(주)", "").replace("(유)", "");
+                            String address = getAddress(name);
                             String logo = content.findElement(By.cssSelector("div > div > div > a > img")).getAttribute("src");
                             result.add(new Company(name, address, logo));
                         }
@@ -104,12 +110,13 @@ public class CrawlerService {
             /*
              * 마지막 페이지 크롤링
              */
+            System.out.println(driver.getCurrentUrl());
             driver.navigate().to(driver.getCurrentUrl());
             contents = driver.findElements(By.cssSelector("div.section_group > section"));
             if (contents.size() > 0) {
                 for (WebElement content : contents) {
-                    String name = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dt > a")).getText().replace("(주)", "");
-                    String address = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dd:nth-child(2) > span:nth-child(3)")).getText();
+                    String name = content.findElement(By.cssSelector("div > div > dl.content_col2_3.cominfo > dt > a")).getText().replace("(주)", "").replace("(유)", "");
+                    String address = getAddress(name);
                     String logo = content.findElement(By.cssSelector("div > div > div > a > img")).getAttribute("src");
                     result.add(new Company(name, address, logo));
                 }
@@ -133,5 +140,54 @@ public class CrawlerService {
             }
         }
         return true;
+    }
+
+    /*
+     *  회사 주소 크롤링
+     */
+    public String getAddress(String str) {
+        String address = null;
+
+        log.info("####Address Crawler START####");
+
+        Path path = Paths.get(driver);
+        System.setProperty("webdriver.chrome.driver", path.toString());
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-popup-blocking");   // 팝업 안띄움
+//        options.addArguments("headless");   // 브라우저 안띄움
+        options.addArguments("--disable-gpu");  // gpu 비활성화
+        options.addArguments("--blink-settings=imagesEnabled=false");   // 이미지 다운 안받음
+
+        WebDriver driver = new ChromeDriver(options);
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));    // 드라이버가 실행된 후 10초 기다림
+
+        try {
+            driver.get(addressURL+str);
+
+            webDriverWait.until(
+                    ExpectedConditions.presenceOfElementLocated(By.cssSelector("#company_info_list > div.content > div:nth-child(1)"))
+            );
+
+            WebElement webElement = driver.findElement(By.cssSelector("#company_info_list > div.content > div:nth-child(1)"));
+            String name = webElement.findElement(By.cssSelector("h2 > a")).getText().replace("(주)", "").replace("(유)", "");
+            if (name.equals(str)) {
+                List<WebElement> contents = webElement.findElements(By.cssSelector("div.corp_info > dl"));
+                if (contents.size() > 0) {
+                    for (WebElement content : contents) {
+                        if(content.findElement(By.cssSelector("dt")).getText().equals("기업주소"))
+                            address = content.findElement(By.cssSelector("dd")).getText();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error("Address Crawler ERROR 발생");
+
+        } finally {
+            driver.quit();  // 모든 탭 종료
+            log.info("####Address Crawler END####");
+        }
+        return address;
     }
 }
