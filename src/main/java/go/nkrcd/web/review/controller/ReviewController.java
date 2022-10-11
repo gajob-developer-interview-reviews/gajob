7184711007a6662b112512f5b9f96ccbae4bc6c1
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2022-10-11 15:49:56 +0900

    fix : 후기 등록(982768c)
    @Controller 에서는 @ResponseBody 이용해서 할 수 있음
    
    - 변경 필요 부분
     : ReviewRestController -> ReviewController 로 이동 후 @ResponseBody 를 이용해 데이터를 반환하도록 함

--- src/main/java/go/nkrcd/web/review/controller/ReviewController.java
+++ src/main/java/go/nkrcd/web/review/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package go.nkrcd.web.review.controller;
 
 import go.nkrcd.web.main.model.Code;
+import go.nkrcd.web.main.model.RestEntity;
 import go.nkrcd.web.main.model.User;
 import go.nkrcd.web.main.repository.CodeRepository;
 import go.nkrcd.web.main.service.UserService;
@@ -8,11 +9,14 @@ import go.nkrcd.web.review.model.AddReview;
 import go.nkrcd.web.review.model.Level;
 import go.nkrcd.web.review.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -21,6 +25,12 @@ import java.util.List;
 public class ReviewController {
     @Autowired
     CodeRepository codeRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    ReviewService reviewService;
 
     /*
      * 마이페이지 > 후기 작성하기
@@ -35,6 +45,21 @@ public class ReviewController {
         model.addAttribute("codes", codes);
 
         return "review";
+    }
+
+    /*
+     * 마이페이지 > 후기 작성하기 > 후기 등록하기
+     */
+    @ResponseBody
+    @RequestMapping(value = "", method = {RequestMethod.POST})
+    public ResponseEntity save(AddReview addReview, Authentication authentication) {
+        User user = userService.findUserByOauthId(authentication.getName());
+        try {
+            reviewService.save(addReview, user);
+        } catch (RuntimeException e) {
+            return new ResponseEntity(RestEntity.res(HttpStatus.INTERNAL_SERVER_ERROR, "후기가 등록에 실패하였습니다.", null), HttpStatus.OK);
+        }
+        return new ResponseEntity(RestEntity.res(HttpStatus.OK, "후기가 등록되었습니다.", null), HttpStatus.OK);
     }
 
 }
