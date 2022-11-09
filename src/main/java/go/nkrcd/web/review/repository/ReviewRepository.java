79f95600095e0ac2a980f1c30531817dbe04511b
yewo2nn16 <80824750+yewon-Noh@users.noreply.github.com>
2022-11-09 09:58:08 +0900

    fix : 쿼리 불필요 like -> = 수정

--- src/main/java/go/nkrcd/web/review/repository/ReviewRepository.java
+++ src/main/java/go/nkrcd/web/review/repository/ReviewRepository.java
@@ -14,21 +14,21 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    @Query("select r from Review r where r.company.cId like %:cId% and r.delYn = 'N' order by r.writeDt desc ")
+    @Query("select r from Review r where r.company.cId = :cId and r.delYn = 'N' order by r.writeDt desc ")
     List<Review> findReviews(@Param("cId") String cId);
 
     @Query("select count(1)" +
             "from Review r " +
-            "where r.company.cId like %:cId% and r.delYn = 'N'")
+            "where r.company.cId = :cId and r.delYn = 'N'")
     List<Long> findReviewCount(@Param("cId") String cId);
 
     @Query("select new go.nkrcd.web.review.model.LevelRate(r.level as level, COUNT(1) as count)" +
             "from Review r " +
-            "where r.company.cId like %:cId% and r.delYn = 'N'" +
+            "where r.company.cId = :cId and r.delYn = 'N'" +
             "group by r.level")
     List<LevelRate> findLevelRate(@Param("cId") String cId);
 
-    @Query("select r.rid from Review r where r.company.cId like %:cId% and r.delYn = 'N'")
+    @Query("select r.rid from Review r where r.company.cId = :cId and r.delYn = 'N'")
     List<Long> findRIds(@Param("cId") String cId);
 
     @Query("select r from Review r where r.user.uid = :uid and r.delYn ='N' order by r.rid desc ")
